021c7d38e9b70da5d7296fe89d55969ced3eeb66
fhabhishektiwari <abhishektiwarifh@gmail.com>
2022-08-03 14:37:59 +0530

    send Mail from local server

---
+++ sendmail.js
@@ -0,0 +1,26 @@
+const nodeMailer=require("nodemailer");
+var transport=nodeMailer.createTransport({
+    host:"smtp.gmail.com",
+    port:587,
+    secure:false,
+    requireTLS:true,
+    auth:{
+        user:'abhishektiwarifh@gmail.com',
+        pass:'Abhishek@Tiwari918',
+    }
+})
+
+var mailOptions={
+    from:'abhishektiwarifh@gmail.com',
+    to:'abhishektiwarifh@gmail.com',
+    subject:'test node mail module',
+    text:'Hello Abhishek Tiwari, Welcome to js world!'
+}
+
+transport.sendMail(mailOptions,function(error,info){
+    if(error){
+        console.log(error);
+    }else{
+        console.log("email has been sent",info.response)
+    }
+})
