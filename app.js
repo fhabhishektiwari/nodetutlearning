90aa84c7aa362c06bf8a2eb05d25f3646f31ca0b
fhabhishektiwari <abhishektiwarifh@gmail.com>
2022-08-03 18:48:15 +0530

    added middleware specific routing

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express=require('express');
 const app=express();
+const router=express.Router();
 
 /* Middleware: Middleware functions are functions that have access to the request object (req), the response object (res), and the next middleware function in the application's request-response cycle. These functions are used to modify req and res objects for tasks like parsing request bodies, adding response headers, etc*/
 
@@ -7,33 +8,18 @@ const checkUrl=(req,res,next)=>{
     console.log("Current route is ",req.originalUrl);
     next();
 }
-app.use(checkUrl);
+// app.use(checkUrl);
 
 
 app.get('/',(req,res)=>{
     res.send("Home Page");
 });
-app.get('/about',(req,res)=>{
+
+router.get('/about',checkUrl,(req,res)=>{
     res.send("About Page");
 });
-app.get('/contact-us',(req,res)=>{
-    res.send("Contact Page");
-});
-app.get('/login',(req,res)=>{
+router.get('/login',checkUrl,(req,res)=>{
     res.send("Login Page");
-})
-app.put('/put',(req,res)=>{
-    res.send("put request");
-})
-app.post('/post',(req,res)=>{
-    res.send("post request");
 });
-app.delete('/delete',(req,res)=>{
-    res.send("delete request");
-})
-
-app.get('/*',(req,res)=>{
-    res.send("404 Page Not Found!");
-})
-
+app.use("/",router);
 app.listen(3000);
