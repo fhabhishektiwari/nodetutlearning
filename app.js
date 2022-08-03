fe6464d8a661bb9b9e84eb06bc04d292fe2683c3
fhabhishektiwari <abhishektiwarifh@gmail.com>
2022-08-03 18:22:45 +0530

    Routing with express js framework

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
 const express=require('express');
 const app=express();
 app.get('/',(req,res)=>{
-    res.send("Hello express js...");
-}).listen(3000);
+    res.send("Home Page");
+});
+app.get('/about',(req,res)=>{
+    res.send("About Page");
+});
+app.get('/contact-us',(req,res)=>{
+    res.send("Contact Page");
+});
+app.get('/login',(req,res)=>{
+    res.send("Login Page");
+})
+app.put('/put',(req,res)=>{
+    res.send("put request");
+})
+app.post('/post',(req,res)=>{
+    res.send("post request");
+});
+app.delete('/delete',(req,res)=>{
+    res.send("delete request");
+})
+
+app.get('/*',(req,res)=>{
+    res.send("404 Page Not Found!");
+})
+
+app.listen(3000);
