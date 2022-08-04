35500409b48d98a9379eed9d7a9ed3a6be99aaf3
fhabhishektiwari <abhishektiwarifh@gmail.com>
2022-08-04 14:29:18 +0530

    Dynamic Content

--- templateengineejs.js
+++ templateengineejs.js
@@ -2,8 +2,17 @@ const express=require('express');
 const app=express();
 // template engine(ejs)//npm i ejs
 app.set('view engine','ejs');
-app.get("/profile/:name",(req,res)=>{
-    console.log(req.params.name);
-    res.render("Profile",{name:req.params.name});
+// app.get("/profile/:name",(req,res)=>{
+//     // console.log(req.params.name);
+//     data={devname:"Nishant",email:"nishant.bhatnagar@firsthive.com",skills:['Java','Python','Php','node js','react js']};
+//     res.render("Profile",{name:req.params.name,data:data});
+// })
+
+app.get("/profile",(req,res)=>{
+    data={devname:"Nishant",email:"nishant.bhatnagar@firsthive.com",skills:['Java','Python','Php','node js','react js']};
+
+    res.render('Profile',{name:data.devname,data:data});
+    
 })
+
 app.listen(3000);
