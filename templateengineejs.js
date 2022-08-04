119b6100031ffd39f0e8770cb89166118cda44de
fhabhishektiwari <abhishektiwarifh@gmail.com>
2022-08-04 15:16:23 +0530

    make common header

--- templateengineejs.js
+++ templateengineejs.js
@@ -2,17 +2,17 @@ const express=require('express');
 const app=express();
 // template engine(ejs)//npm i ejs
 app.set('view engine','ejs');
-// app.get("/profile/:name",(req,res)=>{
+app.get("/profile/:name",(req,res)=>{
 //     // console.log(req.params.name);
-//     data={devname:"Nishant",email:"nishant.bhatnagar@firsthive.com",skills:['Java','Python','Php','node js','react js']};
-//     res.render("Profile",{name:req.params.name,data:data});
-// })
-
-app.get("/profile",(req,res)=>{
     data={devname:"Nishant",email:"nishant.bhatnagar@firsthive.com",skills:['Java','Python','Php','node js','react js']};
+    res.render("Profile",{name:req.params.name,data:data});
+})
 
-    res.render('Profile',{name:data.devname,data:data});
-    
+app.get("/",(req,res)=>{
+    res.render('Home');//here give file name Home.ejs  
+})
+app.get("/login",(req,res)=>{
+    res.render('Login');//here give file name Login.ejs  
 })
 
 app.listen(3000);
